fc6d59e90c88867faca3f058077d9b29a74ab1de
TBence2002 <torokbencee.2002@gmail.com>
2023-12-29 16:46:30 +0100

    Módosítások

--- src/pages/MyGallery.js
+++ src/pages/MyGallery.js
@@ -1,25 +1,10 @@
-import React, { useState, useEffect } from 'react'
-import { storage } from '../components/gallery-components/Firebase'
-import { ref, listAll, getDownloadURL} from 'firebase/storage'
+import React from 'react'
 import "../components/gallery-components/Gallery.css"
 import { motion } from 'framer-motion'
 import 'photoswipe/dist/photoswipe.css'
-import { Gallery, Item } from 'react-photoswipe-gallery'
+
 
 function MyGallery() {
-  const [imageList, setImageList] = useState([]);
-
-  const imageListRef = ref(storage, "gallery/");
-  useEffect(() => {
-    listAll(imageListRef).then((response) => {
-      const promises = response.items.map((item) => {
-        return getDownloadURL(item);
-      });
-      Promise.all(promises).then((urls) => {
-        setImageList(urls);
-      });
-    });
-  }, [imageList, imageListRef]);
 
   return (
     <motion.div className='gallery-container'
@@ -33,22 +18,7 @@ function MyGallery() {
       }}>
       <h1>Galéria</h1>
       <div className='image-container'>
-       
-        {imageList.map((url) => {
-          return <div className='image'>
-            <Gallery>
-              <Item
-                original={url}
-                thumbnail={url}
-                width="1024"
-                height="768">
-                {({ ref, open }) => (
-                  <img ref={ref} onClick={open} src={url} alt="kép"/>
-                )}
-              </Item>
-            </Gallery>
-          </div>;
-        })}
+       <h3>Jelenleg nem tartalmaz képet a galéria.</h3>
       </div>
     </motion.div>
   )
