3e56392605d77b09b5aec3c7ffcf2dc7aa27c472
TBence2002 <torokbencee.2002@gmail.com>
2023-12-29 15:32:58 +0100

    Módosítások

--- src/components/gallery-components/Firebase.jsx
+++ src/components/gallery-components/Firebase.jsx
@@ -1,14 +1,19 @@
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import {getStorage} from 'firebase/storage'
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCb6XY6hhiDzyrwMy1LqxIhEkilxuyc8Tk",
-  authDomain: "uploadingpicture-29742.firebaseapp.com",
-  projectId: "uploadingpicture-29742",
-  storageBucket: "uploadingpicture-29742.appspot.com",
-  messagingSenderId: "193259266787",
-  appId: "1:193259266787:web:22db6afd4e036862c5dbd6"
+  apiKey: "AIzaSyAmAlJEkUSvwFVapFIp2Rl2Ab88cOcvwrg",
+  authDomain: "patak-parlat-gallery.firebaseapp.com",
+  projectId: "patak-parlat-gallery",
+  storageBucket: "patak-parlat-gallery.appspot.com",
+  messagingSenderId: "537508907067",
+  appId: "1:537508907067:web:6f49d4c5f69edc38f3b41e"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const storage = getStorage(app);
