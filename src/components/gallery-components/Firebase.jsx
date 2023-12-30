c0e0c64634ffe5cd2d546ef92684de9f30417fe7
TBence2002 <torokbencee.2002@gmail.com>
2023-12-30 20:00:55 +0100

    Galéria

--- src/components/gallery-components/Firebase.jsx
+++ src/components/gallery-components/Firebase.jsx
@@ -1,19 +1,15 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import {getStorage} from 'firebase/storage'
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAmAlJEkUSvwFVapFIp2Rl2Ab88cOcvwrg",
-  authDomain: "patak-parlat-gallery.firebaseapp.com",
-  projectId: "patak-parlat-gallery",
-  storageBucket: "patak-parlat-gallery.appspot.com",
-  messagingSenderId: "537508907067",
-  appId: "1:537508907067:web:6f49d4c5f69edc38f3b41e"
+  apiKey: "AIzaSyBOrkYzjAXbfFHG1yv0_TcTXayuQD7NCpQ",
+  authDomain: "parlat-91919.firebaseapp.com",
+  projectId: "parlat-91919",
+  storageBucket: "parlat-91919.appspot.com",
+  messagingSenderId: "1077668769686",
+  appId: "1:1077668769686:web:79bef8f59f6a667411f7b7"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const storage = getStorage(app);
